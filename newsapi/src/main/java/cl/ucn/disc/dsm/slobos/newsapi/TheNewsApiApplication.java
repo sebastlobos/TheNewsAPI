d7ba139d7aad86a0546b11227efab0f49cc517c9
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-12-14 03:00:38 -0300

    first commit

---
+++ newsapi/src/main/java/cl/ucn/disc/dsm/slobos/newsapi/TheNewsApiApplication.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.slobos.newsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNewsApiApplication.class, args);
+	}
+
+}
---
+++ newsapi/src/test/java/cl/ucn/disc/dsm/slobos/newsapi/TheNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.slobos.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
