428493ebe153852b08b5f92cbb015ad9baba19bf
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-12-18 23:50:40 -0300

    chore(project)= initialize

--- newsapi/src/main/java/cl/ucn/disc/dsm/slobos/newsapi/TheNewsApiApplication.java
+++ newsapi/src/main/java/cl/ucn/disc/dsm/slobos/newsapi/TheNewsApiApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheNewsApiApplication {
+	/**
+	 *  The Main starting point.
+	 *
+	 * @param args to use
+	 */
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheNewsApiApplication.class, args);
